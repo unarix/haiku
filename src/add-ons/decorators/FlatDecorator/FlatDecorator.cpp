95aec0fd1a1541fa4fd1edc3698a4424b72a4558
Nhtello <nahuel.tello@enel.com>
2026-06-19 16:30:05 -0300

    fborder

--- src/add-ons/decorators/FlatDecorator/FlatDecorator.cpp
+++ src/add-ons/decorators/FlatDecorator/FlatDecorator.cpp
@@ -243,7 +243,7 @@ FlatDecorator::GetComponentColors(Component component, uint8 highlight,
 				_colors[2] = tint_color(fNonFocusFrameColor, B_NO_TINT);
 				_colors[3] = tint_color(fNonFocusFrameColor, B_NO_TINT);
 				_colors[4] = tint_color(fNonFocusFrameColor, 1.05);
-				_colors[5] = tint_color(fNonFocusFrameColor, 1.0);
+				_colors[5] = (rgb_color){255, 0, 0, 255};
 				_colors[6] = tint_color(fNonFocusTabColor, 1.2);
 			}
 			if (highlight == HIGHLIGHT_RESIZE_BORDER
@@ -500,7 +500,7 @@ FlatDecorator::_DrawFrame(BRect rect)
 			ComponentColors colors;
 			_GetComponentColors(COMPONENT_LEFT_BORDER, colors, fTopTab);
 
-			fDrawingEngine->StrokeRect(r, (rgb_color){255, 0, 0, 255});
+			fDrawingEngine->StrokeRect(r, colors[5]);
 			break;
 		}
 
