a6d1e9e86a2d48e767168e49bc5d6eff3a9d22d7
Nhtello <nahuel.tello@enel.com>
2026-07-06 19:13:16 -0300

    +40

--- src/preferences/appearance/AntialiasingSettingsView.cpp
+++ src/preferences/appearance/AntialiasingSettingsView.cpp
@@ -109,7 +109,7 @@ AntialiasingSettingsView::AntialiasingSettingsView(const char* name)
 	fMonospacedCheckBox = new BCheckBox("monospaced", B_TRANSLATE("Fixed font only"),
 		new BMessage(kMsgSetHintingMonospaced));
 	fMonospacedCheckBox->SetExplicitMinSize(
-		BSize(fMonospacedCheckBox->StringWidth(fMonospacedCheckBox->Label()) + 30,
+		BSize(fMonospacedCheckBox->StringWidth(fMonospacedCheckBox->Label()) + 40,
 		B_SIZE_UNSET));
 
 	BLayoutBuilder::Group<>(this, B_VERTICAL, B_USE_DEFAULT_SPACING)
